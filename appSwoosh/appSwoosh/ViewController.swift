5970c9a513236fd099e962e1ec88ba488f3d5e1a
guitarkeegan <keegananglim@gmail.com>
2018-11-12 12:32:19 -0800

    example of pixel perfect layout

--- appSwoosh/appSwoosh/ViewController.swift
+++ appSwoosh/appSwoosh/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BGImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // to center the swoosh logo programatically
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        BGImage.frame = view.frame;
     }
 
 
