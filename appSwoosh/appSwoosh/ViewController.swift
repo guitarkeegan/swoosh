7ff4caa6e984be39484512ac8b79d19188584e98
guitarkeegan <keegananglim@gmail.com>
2018-11-12 10:54:42 -0800

    added welcome images

---
+++ appSwoosh/appSwoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appSwoosh
+//
+//  Created by Keegan Anglim on 11/12/18.
+//  Copyright © 2018 Keegan Anglim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
