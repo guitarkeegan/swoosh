1840765398e9af8f6f8d0c0c61bc61e2c9cf5cfb
guitarkeegan <keegananglim@gmail.com>
2018-11-12 18:15:50 -0800

    attempted setting contraints on GO ALL IN

--- appSwoosh/appSwoosh/ViewController.swift
+++ appSwoosh/appSwoosh/ViewController.swift
@@ -15,10 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // to center the swoosh logo programatically
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        BGImage.frame = view.frame;
     }
 
 
